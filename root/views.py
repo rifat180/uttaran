fd9283d06fde2af634b0a0785f101b92b89a049d
Md. Rifaet - Ullah <rifat@localhost.localdomain>
2019-06-02 01:49:51 +0600

    Added navbar and style

--- root/views.py
+++ root/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     template_name = 'root/home.html'
-    context = {}
+    context = {
+        'title': 'Welcome | Uttron',
+        'name': 'Uttron'
+    }
     return render(request, template_name, context)
     
