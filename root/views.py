8225dc98ff2f58e67bdd02f7efa3cc7d2fe98522
Md. Rifaet - Ullah <rifat@localhost.localdomain>
2019-06-02 03:42:13 +0600

    Trying to show uploaded picture on testimonial section

--- root/views.py
+++ root/views.py
@@ -1,41 +1,66 @@
 from django.shortcuts import render
+from .models import (
+    LibraryName,
+    Mission,
+    Vision,
+    Goal,
+    Testimonial,
+    EventPicture,
+    Book,
+    Member,
+    About
+)
 
 # Create your views here.
 def home(request):
     template_name = 'root/home.html'
     context = {
         'title': 'Welcome | Uttron',
-        'name': 'Uttron',
+        'name': LibraryName.objects.all()[-1],
         'active_link': 'home',
-        'testi_len': 3,
-        'length': 3,
+        'missions': Mission.objects.all(),
+        'visions': Vision.objects.all(),
+        'goals': Goal.objects.all(),
+        'testimonials': Testimonial.objects.all(),
+        'testi_len': len(Testimonial.objects.all()),
+        'event_pictures': EventPicture.objects.all(),
+        'length': len(EventPicture.objects.all())
     }
     return render(request, template_name, context)
 
 def books(request):
     template_name = 'root/books.html'
     context = {
-        'title': 'Welcome | Uttron',
-        'name': 'Uttron',
+        'title': 'Books | Uttron',
+        'name': LibraryName.objects.all()[-1],
         'active_link': 'books',
+        'all_books': Book.objects.all()
     }
     return render(request, template_name, context)
 
 def book_details(request):
     template_name = 'root/book_details.html'
     context = {
-        'title': 'Welcome | Uttron',
-        'name': 'Uttron',
+        'title': 'Book Details | Uttron',
+        'name': LibraryName.objects.all()[0],
         'active_link': 'books',
+        'book': Book.objects.get(pk=id)
     }
     return render(request, template_name, context)
 
 def members(request):
     template_name = 'root/members.html'
+    all_members = Member.objects.all()
+    advisors = [ member for member in all_members if member.designation.lower() == 'advisor' ]
+    committee = [member for member in all_members if member.designation.lower() == 'committee member']
+    members = [member for member in all_members if member.designation.lower()== 'member']
     context = {
-        'title': 'Welcome | Uttron',
-        'name': 'Uttron',
+        'title': 'Members | Uttron',
+        'name': LibraryName.objects.all()[-1],
         'active_link': 'members',
+        'advisors': advisors,
+        'committee': committee,
+        'members': members
     }
     return render(request, template_name, context)
 
