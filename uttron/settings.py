09a75889fa176dd039d11a38ac295addd89ae09d
Md. Rifaet - Ullah <rifat@localhost.localdomain>
2019-06-03 03:30:19 +0600

    Trying debug false

--- uttron/settings.py
+++ uttron/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v%u=#v%3p&s-abm!x&+)n7=!snka-i-nvktzvio=b5)wff%)mo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
