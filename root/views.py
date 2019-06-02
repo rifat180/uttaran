14741912ef3b95727c7f8698ba3cd2d669362e3c
Md. Rifaet - Ullah <rifat@localhost.localdomain>
2019-06-03 03:26:06 +0600

    Made the website responsive

--- root/views.py
+++ root/views.py
@@ -15,14 +15,14 @@ from .models import (
 def home(request):
     template_name = 'root/home.html'
     name = LibraryName.objects.all()
-    name = name[0].name if name else 'Uttron'
+    name = name[0].name if name else 'Uttaran'
     missions = Mission.objects.all()
     visions = Vision.objects.all()
     goals = Goal.objects.all()
     testimonials = Testimonial.objects.all()
     event_pictures = EventPicture.objects.all()
     context = {
-        'title': 'Welcome | Uttron',
+        'title': 'Welcome | ' + name,
         'name': name,
         'active_link': 'home',
         'missions': missions,
@@ -38,10 +38,10 @@ def home(request):
 def books(request):
     template_name = 'root/books.html'
     name = LibraryName.objects.all()
-    name = name[0].name if name else 'Uttron'
+    name = name[0].name if name else 'Uttaran'
     all_books = Book.objects.all()
     context = {
-        'title': 'Books | Uttron',
+        'title': 'Books | ' + name,
         'name': name,
         'active_link': 'books',
         'all_books': all_books
@@ -51,9 +51,9 @@ def books(request):
 def book_details(request, id):
     template_name = 'root/book_details.html'
     name = LibraryName.objects.all()
-    name = name[0].name if name else 'Uttron'
+    name = name[0].name if name else 'Uttaran'
     context = {
-        'title': 'Book Details | Uttron',
+        'title': 'Book Details | ' + name,
         'name': name,
         'active_link': 'books',
         'book': Book.objects.get(pk=id)
@@ -63,13 +63,13 @@ def book_details(request, id):
 def members(request):
     template_name = 'root/members.html'
     name = LibraryName.objects.all()
-    name = name[0].name if name else 'Uttron'
+    name = name[0].name if name else 'Uttaran'
     all_members = Member.objects.all()
     advisors = [ member for member in all_members if member.designation.lower() == 'advisor' ]
     committee = [member for member in all_members if member.designation.lower() == 'committee member']
     members = [member for member in all_members if member.designation.lower()== 'member']
     context = {
-        'title': 'Members | Uttron',
+        'title': 'Members | ' + name,
         'name': name,
         'active_link': 'members',
         'advisors': advisors,
@@ -81,9 +81,9 @@ def members(request):
 def member_details(request, id):
     template_name = 'root/member_details.html'
     name = LibraryName.objects.all()
-    name = name[0].name if name else 'Uttron'
+    name = name[0].name if name else 'Uttaran'
     context = {
-        'title': 'Member Details | Uttron',
+        'title': 'Member Details | ' + name,
         'name': name,
         'active_link': 'members',
         'member': Member.objects.get(pk = id)
@@ -93,10 +93,10 @@ def member_details(request, id):
 def about(request):
     template_name = 'root/about.html'
     name = LibraryName.objects.all()
-    name = name[0].name if name else 'Uttron'
+    name = name[0].name if name else 'Uttaran'
     paragraph = About.objects.all()
     context = {
-        'title': 'About | Uttron',
+        'title': 'About | ' + name,
         'name': name,
         'active_link': 'about',
         'paragraphs': paragraph,
@@ -106,9 +106,9 @@ def about(request):
 def contacts(request):
     template_name = 'root/contacts.html'
     name = LibraryName.objects.all()
-    name = name[0].name if name else 'Uttron'
+    name = name[0].name if name else 'Uttaran'
     context = {
-        'title': 'Contacts | Uttron',
+        'title': 'Contacts | ' + name,
         'name': name,
         'active_link': 'contacts',
     }
